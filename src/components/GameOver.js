b93d07f7f02b6873d2849b98599fbf969aeac917
Michelle da Rosa Machado <machado.profile@gmail.com>
2022-08-11 16:29:03 -0300

    jogo da memoria pronto

---
+++ src/components/GameOver.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function GameOver(props) {
+    return (props.show?
+
+        <div id="gameOver">
+
+            <div>
+                Parabéns, você completou o jogo!
+            </div>
+
+            <button id="restart" onClick={props.handleRestart}>Jogue Novamente</button>
+        </div>:<></>
+    )
+}
